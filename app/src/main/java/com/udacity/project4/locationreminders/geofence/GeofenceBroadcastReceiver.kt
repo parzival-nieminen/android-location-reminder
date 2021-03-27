84b446ca0f29dc3dfffa79a04677b6568cf5887c
Parzival <parzival.nieminen@gmail.com>
2021-03-27 20:26:15 +0100

    feat add geo broadcaster and setup notification
    * impl. GeofenceBroadcastReceiver.kt
    * impl. GeofenceTransitionsJobIntentService.kt

--- app/src/main/java/com/udacity/project4/locationreminders/geofence/GeofenceBroadcastReceiver.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/geofence/GeofenceBroadcastReceiver.kt
@@ -3,6 +3,9 @@ package com.udacity.project4.locationreminders.geofence
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import com.google.android.gms.location.Geofence
+import com.google.android.gms.location.GeofencingEvent
+import com.udacity.project4.locationreminders.savereminder.SaveReminderFragment
 
 /**
  * Triggered by the Geofence.  Since we can have many Geofences at once, we pull the request
@@ -16,8 +19,12 @@ import android.content.Intent
 
 class GeofenceBroadcastReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action == SaveReminderFragment.ACTION_GEOFENCE_EVENT) {
+            val geoEvent = GeofencingEvent.fromIntent(intent)
 
-//TODO: implement the onReceive method to receive the geofencing events at the background
-
+            if (geoEvent.geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER) {
+                GeofenceTransitionsJobIntentService.enqueueWork(context, intent)
+            }
+        }
     }
 }
--- app/src/main/java/com/udacity/project4/locationreminders/geofence/GeofenceTransitionsJobIntentService.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/geofence/GeofenceTransitionsJobIntentService.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import androidx.core.app.JobIntentService
 import com.google.android.gms.location.Geofence
+import com.google.android.gms.location.GeofencingEvent
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
 import com.udacity.project4.locationreminders.data.local.RemindersLocalRepository
@@ -22,7 +23,6 @@ class GeofenceTransitionsJobIntentService : JobIntentService(), CoroutineScope {
     companion object {
         private const val JOB_ID = 573
 
-        //        TODO: call this to start the JobIntentService to handle the geofencing transition events
         fun enqueueWork(context: Context, intent: Intent) {
             enqueueWork(
                 context,
@@ -33,18 +33,16 @@ class GeofenceTransitionsJobIntentService : JobIntentService(), CoroutineScope {
     }
 
     override fun onHandleWork(intent: Intent) {
-        //TODO: handle the geofencing transition events and
-        // send a notification to the user when he enters the geofence area
-        //TODO call @sendNotification
+        val geoEvent = GeofencingEvent.fromIntent(intent)
+        geoEvent.triggeringGeofences.forEach(::sendNotification)
     }
 
-    //TODO: get the request id of the current geofence
-    private fun sendNotification(triggeringGeofences: List<Geofence>) {
-        val requestId = ""
+    private fun sendNotification(geo: Geofence) {
+        val requestId = geo.requestId
 
         //Get the local repository instance
         val remindersLocalRepository: RemindersLocalRepository by inject()
-//        Interaction to the repository has to be through a coroutine scope
+        //Interaction to the repository has to be through a coroutine scope
         CoroutineScope(coroutineContext).launch(SupervisorJob()) {
             //get the reminder with the request id
             val result = remindersLocalRepository.getReminder(requestId)
