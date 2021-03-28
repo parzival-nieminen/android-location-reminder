0cde28607343b127c5986e6e2c75e1b7506889c3
Parzival <parzival.nieminen@gmail.com>
2021-03-28 22:03:44 +0200

    feat add integration test
    * add integration tests

--- app/src/androidTest/java/com/udacity/project4/RemindersActivityTest.kt
+++ app/src/androidTest/java/com/udacity/project4/RemindersActivityTest.kt
@@ -1,9 +1,17 @@
 package com.udacity.project4
 
 import android.app.Application
+import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.replaceText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import com.udacity.project4.locationreminders.RemindersActivity
 import com.udacity.project4.locationreminders.data.ReminderDataSource
 import com.udacity.project4.locationreminders.data.local.LocalDB
 import com.udacity.project4.locationreminders.data.local.RemindersLocalRepository
@@ -11,6 +19,8 @@ import com.udacity.project4.locationreminders.reminderslist.RemindersListViewMod
 import com.udacity.project4.locationreminders.savereminder.SaveReminderViewModel
 import kotlinx.coroutines.runBlocking
 import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.startKoin
@@ -25,8 +35,9 @@ import org.koin.test.get
 class RemindersActivityTest :
     AutoCloseKoinTest() {// Extended Koin Test - embed autoclose @after method to close Koin after every test
 
-    private lateinit var repository: ReminderDataSource
-    private lateinit var appContext: Application
+    private lateinit var db: ReminderDataSource
+    private lateinit var context: Application
+    @get:Rule var activityTestRule: ActivityTestRule<RemindersActivity> = ActivityTestRule(RemindersActivity::class.java)
 
     /**
      * As we use Koin as a Service Locator Library to develop our code, we'll also use Koin to test our code.
@@ -35,37 +46,66 @@ class RemindersActivityTest :
     @Before
     fun init() {
         stopKoin()//stop the original app koin
-        appContext = getApplicationContext()
+        context = getApplicationContext()
         val myModule = module {
             viewModel {
                 RemindersListViewModel(
-                    appContext,
+                    context,
                     get() as ReminderDataSource
                 )
             }
             single {
                 SaveReminderViewModel(
-                    appContext,
+                    context,
                     get() as ReminderDataSource
                 )
             }
             single { RemindersLocalRepository(get()) as ReminderDataSource }
-            single { LocalDB.createRemindersDao(appContext) }
+            single { LocalDB.createRemindersDao(context) }
         }
         //declare a new koin module
         startKoin {
             modules(listOf(myModule))
         }
         //Get our real repository
-        repository = get()
+        db = get()
 
         //clear the data to start fresh
         runBlocking {
-            repository.deleteAllReminders()
+            db.deleteAllReminders()
         }
     }
 
+    @Test
+    fun when_title_is_missing_in_reminder_then_show_missing_snackbar_info() {
 
-//    TODO: add End to End testing to the app
+        val scenario = ActivityScenario.launch(RemindersActivity::class.java)
+        onView(withId(R.id.noDataTextView)).check(matches(isDisplayed()))
+        onView(withId(R.id.addReminderFAB)).perform(click())
+        onView(withId(R.id.reminderDescription)).perform(replaceText("World"))
+        onView(withId(R.id.saveReminder)).perform(click())
+
+        onView(withId(com.google.android.material.R.id.snackbar_text))
+            .check(matches(withText(R.string.err_enter_title)))
+
+        scenario.close()
+    }
+
+    @Test
+    fun when_location_is_missing_in_reminder_then_show_missing_snackbar_info() {
+
+        val scenario = ActivityScenario.launch(RemindersActivity::class.java)
+        onView(withId(R.id.noDataTextView)).check(matches(isDisplayed()))
+        onView(withId(R.id.addReminderFAB)).perform(click())
+        onView(withId(R.id.reminderTitle)).perform(replaceText("Hello"))
+        onView(withId(R.id.reminderDescription)).perform(replaceText("World"))
+        onView(withId(R.id.saveReminder)).perform(click())
+
+        onView(withId(com.google.android.material.R.id.snackbar_text))
+            .check(matches(withText(R.string.err_select_location)))
+
+        scenario.close()
+    }
 
 }
+
